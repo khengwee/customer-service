19d775d880d75b35440a5dffadaf66378bc39ead
khengwee <Kheng-Wee.Lai@sc.com>
2019-01-10 18:24:28 +0800

    Fix the basic authentication

---
+++ src/main/java/com/kiwi/customer/config/WebSecurityConfig.java
@@ -0,0 +1,42 @@
+package com.kiwi.customer.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@EnableWebFluxSecurity
+public class WebSecurityConfig {
+
+    @Value("${spring.security.user.name}")
+    private String username;
+
+    @Value("${spring.security.user.password}")
+    private String password;
+
+
+    @Bean
+    SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf().disable()
+                .authorizeExchange()
+                .pathMatchers("/*").permitAll()
+                .pathMatchers("/api/*/**").authenticated()
+                .and().httpBasic()
+                .and()
+                .build();
+    }
+
+    @Bean
+    ReactiveUserDetailsService userDetailService() {
+        User.UserBuilder user = User.withUsername(username)
+                .roles("USER")
+                .password("{noop}"+password);
+
+        return new MapReactiveUserDetailsService(user.build());
+    }
+}
